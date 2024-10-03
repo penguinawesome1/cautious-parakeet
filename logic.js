c4bf85c73639136186e8dd27dde800a746dd9cff
penguinawesome1 <owen.h.colley@gmail.com>
2024-10-03 14:21:17 -0400

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,4 +1,32 @@
-const button = document.getElementById("button");
-button.onclick = () => {
-    console.log("Toggle");
+let depth = 6;
+
+const btn_game_mode = document.getElementById("game_mode");
+const btn_opponent = document.getElementById("opponent");
+const btn_color = document.getElementById("color");
+const btn_depth = document.getElementById("depth");
+
+btn_game_mode.onclick = () => {
+    if (btn_game_mode.innerHTML=="Chess") btn_game_mode.innerHTML = "Chess960";
+    else btn_game_mode.innerHTML = "Chess";
+}
+
+btn_opponent.onclick = () => {
+    if (btn_opponent.innerHTML=="Player") {
+        btn_opponent.innerHTML = "Engine";
+        btn_color.className = "btn1";
+        btn_depth.className = "btn1";
+    } else {
+        btn_opponent.innerHTML = "Player";
+        btn_color.className = "btn1_off";
+        btn_depth.className = "btn1_off";
+    }
+}
+btn_color.onclick = () => {
+    if (btn_color.innerHTML=="White") btn_color.innerHTML = "Black";
+    else btn_color.innerHTML = "White";
+}
+
+btn_depth.onclick = () => {
+    btn_depth.innerHTML = "Depth: " + (depth % 6 + 1);
+    ++depth;
 }
